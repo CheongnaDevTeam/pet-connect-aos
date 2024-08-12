9bce71539ac90b6bf81dc9af9d12edbbd2398aa0
kkjang_93 <gusals456@naver.com>
2024-08-12 10:56:24 +0900

    init : clean architecture
    init : clean architecture
    
    - presentation leyer
    - data layer
    - domain layer

---
+++ presentation/src/main/java/com/kkjang/petconnect/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kkjang.petconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kkjang.petconnect.ui.theme.PetconnectandroidappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PetconnectandroidappTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PetconnectandroidappTheme {
+        Greeting("Android")
+    }
+}
