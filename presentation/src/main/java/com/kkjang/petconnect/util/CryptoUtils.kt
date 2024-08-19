b4096c797784151547406e36e66435de1b344cd7
kkjang_93 <gusals456@naver.com>
2024-08-19 15:21:51 +0900

    feature : generateSecretKey
    feature : generateSecretKey

--- presentation/src/main/java/com/kkjang/petconnect/util/CryptoUtils.kt
+++ presentation/src/main/java/com/kkjang/petconnect/util/CryptoUtils.kt
@@ -3,6 +3,8 @@ package com.kkjang.petconnect.util
 import android.util.Base64
 import java.security.Key
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
 
 object CryptoUtils {
@@ -29,4 +31,29 @@ object CryptoUtils {
 
         return String(decryptedValue)
     }
+
+    fun generateSecretKey(): SecretKey {
+        val keyGen = KeyGenerator.getInstance("AES")
+        keyGen.init(128) // 키 길이 설정 (128, 192, 256 중 하나)
+        return keyGen.generateKey()
+    }
+
+    /****
+     *  사용법 예제
+     *
+     *  // 안전한 키 생성 (예제용)
+     *  val secretKey = CryptoUtils.generateSecretKey().encoded
+     *  println("secretKey : $secretKey")
+     *
+     *  // SecretKey를 Base64로 인코딩하지 않고 직접 사용
+     *  // 암호화
+     *  val encryptedValue = CryptoUtils.encrypt(secretKey, "${String Value}")
+     *  println("Encrypted Value: $encryptedValue")
+     *
+     *
+     *  // 복호화
+     *  val decryptedValue = CryptoUtils.decrypt(secretKey, encryptedValue)
+     *  println("Decrypted Value: $decryptedValue")
+     *
+     * **/
 }
