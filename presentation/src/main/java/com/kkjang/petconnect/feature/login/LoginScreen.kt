6a04a9e4af6f57bc0c8ca7ba04cf5513b4ec26cf
kkjang_50 <gusals456@naver.com>
2024-11-19 21:28:16 +0900

    feature : permission screen
    feature : permission screen

--- presentation/src/main/java/com/kkjang/petconnect/feature/login/LoginScreen.kt
+++ presentation/src/main/java/com/kkjang/petconnect/feature/login/LoginScreen.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,16 +27,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kkjang.petconnect.R
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.glide.GlideImage
+import run.piece.dev.refactoring.compose.common.theme.C_FF7878
+import run.piece.dev.refactoring.compose.common.theme.C_FFFFFF
+import run.piece.dev.refactoring.compose.common.theme.G600_8C919F
+import run.piece.dev.refactoring.compose.common.theme.G700_4F4F52
+import run.piece.dev.refactoring.compose.common.theme.G900_141414
 
 @Composable
 fun LoginScreen(
@@ -71,6 +82,7 @@ fun LoginScreen(
                 loginResult?.isSuccess == true -> {
                     Text("Login successful!")
                 }
+
                 loginResult?.isFailure == true -> {
                     Text("Login failed: ${loginResult?.exceptionOrNull()?.message}")
                 }
@@ -110,42 +122,174 @@ fun LoginScreenPreView() {
     Column(
         modifier = Modifier
             .fillMaxSize() // 전체 화면을 채우도록 설정
-            .padding(16.dp),
+            .padding(top = 72.dp, start = 16.dp, end = 16.dp),
         verticalArrangement = Arrangement.SpaceBetween, // 상단과 하단에 공간을 배치
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // 상단 영역
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp), // 상단과 버튼 사이의 공간
-            horizontalAlignment = Alignment.CenterHorizontally
+                .wrapContentHeight()
         ) {
-            Text(
-                text = stringResource(id = R.string.login_start_title)
-            )
-            Spacer(modifier = Modifier.height(16.dp)) // 제목과 버튼 사이에 공간을 추가
-            // 추가적인 이미지나 UI 요소가 여기에 올 수 있습니다.
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    text = stringResource(id = R.string.permission_screen_title),
+                    color = G900_141414,
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        lineHeight = 32.sp,
+                        fontWeight = FontWeight.W600,
+                        fontFamily = FontFamily(
+                            Font(R.font.pretendard_semibold)
+                        )
+                    )
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(top = 32.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp) // 자식 뷰 사이 간격 추가
+            ) {
+                // 동작 및 피트니스
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_dumbbell,
+                    titleText = stringResource(id = R.string.permission_fitness_title),
+                    descriptionText = stringResource(id = R.string.permission_fitness_sub_title)
+                )
+                // 위치
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_mappin,
+                    titleText = stringResource(id = R.string.permission_location_title),
+                    descriptionText = stringResource(id = R.string.permission_location_sub_title)
+                )
+                // 카메라
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_camera,
+                    titleText = stringResource(id = R.string.permission_camera_title),
+                    descriptionText = stringResource(id = R.string.permission_camera_sub_title)
+                )
+
+                // 사진
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_image,
+                    titleText = stringResource(id = R.string.permission_profile_title),
+                    descriptionText = stringResource(id = R.string.permission_profile_sub_title)
+                )
+
+                // 알림
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_bell,
+                    titleText = stringResource(id = R.string.permission_notification_title),
+                    descriptionText = stringResource(id = R.string.permission_notification_sub_title)
+                )
+
+                // 저장
+                ImageWithTextRow(
+                    imageResId = R.drawable.ic_x24_download,
+                    titleText = stringResource(id = R.string.permission_save_title),
+                    descriptionText = stringResource(id = R.string.permission_save_sub_title)
+                )
+            }
         }
 
-        // 하단 영역
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(70.dp)
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null
-                ) { }
-                .align(Alignment.CenterHorizontally) // 중앙 정렬
+        ScreenConfirmButton()
+    }
+}
+
+@Composable
+fun ImageWithTextRow(
+    imageResId: Int,
+    titleText: String,
+    descriptionText: String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min), // Row의 높이를 자식의 최소 높이에 맞춤
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 이미지
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = titleText,
+            modifier = Modifier.size(24.dp)
+        )
+
+        // 텍스트
+        Column(
+            modifier = Modifier // Row의 높이를 텍스트가 채우도록 설정
+                .weight(1f)
+                .padding(start = 16.dp), // 나머지 공간을 차지
+            verticalArrangement = Arrangement.SpaceBetween // 상하 배치
         ) {
-            Image(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .fillMaxHeight(),
-                painter = painterResource(id = R.drawable.kakao_login_medium_wide),
-                contentDescription = "카카오톡 로그인"
+            Text(
+                text = titleText,
+                color = G900_141414,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    lineHeight = 24.sp,
+                    fontWeight = FontWeight.W600,
+                    fontFamily = FontFamily(
+                        Font(R.font.pretendard_semibold)
+                    )
+                )
+            )
+            Text(
+                text = descriptionText,
+                color = G700_4F4F52,
+                style = TextStyle(
+                    fontSize = 13.sp,
+                    lineHeight = 20.sp,
+                    fontWeight = FontWeight.W400,
+                    fontFamily = FontFamily(
+                        Font(R.font.pretendard_regular)
+                    )
+                )
             )
         }
     }
 }
+
+// 확인 버튼
+@Composable
+fun ScreenConfirmButton() {
+    Button(
+        onClick = {
+            // 버튼 클릭 처리
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = C_FF7878,
+            contentColor = C_FFFFFF,
+            disabledContainerColor = G600_8C919F,
+            disabledContentColor = C_FFFFFF,
+        ),
+        shape = RoundedCornerShape(28.dp), // 모서리 둥글기 설정
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp), // 너비를 화면 가득 채움 (선택 사항)
+    ) {
+        Text(
+            text = stringResource(id = R.string.btn_confirm_title),
+            color = C_FFFFFF,
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight.W600,
+                fontFamily = FontFamily(
+                    Font(R.font.pretendard_semibold)
+                )
+            )
+        )
+    }
+}
+
+
--- presentation/src/main/java/com/kkjang/petconnect/ui/theme/Color.kt
+++ presentation/src/main/java/com/kkjang/petconnect/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.kkjang.petconnect.ui.theme
+package run.piece.dev.refactoring.compose.common.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,70 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF131313)
+val White = Color(0xFFFFFFFF)
+
+val Transparent = Color(0x00000000)
+
+// V3 Colors
+val P100_CFF5F4 = Color(0xFFCFF5F4)
+val P200_9FECE9 = Color(0xFF9FECE9)
+val P300_70E2DF = Color(0xFF70E2DF)
+val P400_40D9D4 = Color(0xFF40D9D4)
+val P500_10CFC9 = Color(0xFF10CFC9)
+val P600_0CB2AD = Color(0xFF0CB2AD)
+val P700_078683 = Color(0xFF078683)
+val P800_04615D = Color(0xFF04615D)
+val P900_002928 = Color(0xFF002928)
+
+val S100_D2E9FF = Color(0xFFD2E9FF)
+val S200_A4D4FF = Color(0xFFA4D4FF)
+val S300_77BEFF = Color(0xFF77BEFF)
+val S400_49A9FF = Color(0xFF49A9FF)
+val S500_1C93FF = Color(0xFF1C93FF)
+val S600_0F77D4 = Color(0xFF0F77D4)
+val S700_0B589E = Color(0xFF0B589E)
+val S800_043D70 = Color(0xFF043D70)
+val S900_031B33 = Color(0xFF031B33)
+
+val G50_FDFDFD = Color(0xFFFDFDFD)
+val G100_F9F9F9 = Color(0xFFF9F9F9)
+val G200_F2F3F4 = Color(0xFFF2F3F4)
+val G300_EAECF0 = Color(0xFFEAECF0)
+val G400_DADCE3 = Color(0xFFDADCE3)
+val G500_B8BCC8 = Color(0xFFB8BCC8)
+val G600_8C919F = Color(0xFF8C919F)
+val G700_757983 = Color(0xFF757983)
+val G800_4A4D55 = Color(0xFF4A4D55)
+val G700_4F4F52 = Color(0xFF4F4F52)
+val G900_141414 = Color(0xFF141414)
+
+// Status Colors
+val Status_Color_100 = Color(0xFFFFF2F3F4)
+val Status_Color_90 = Color(0xFFE6F2F3F4)
+val Status_Color_80 = Color(0xFFCCF2F3F4)
+val Status_Color_70 = Color(0xFFB3F2F3F4)
+val Status_Color_60 = Color(0xFF99F2F3F4)
+val Status_Color_50 = Color(0xFF80F2F3F4)
+val Status_Color_40 = Color(0xFF66F2F3F4)
+val Status_Color_30 = Color(0xFF4DF2F3F4)
+val Status_Color_20 = Color(0xFF33F2F3F4)
+val Status_Color_10 = Color(0xFF1AF2F3F4)
+
+val Pie_Chart_Color_A591FF = Color(0xFFA591FF)
+val Pie_Chart_Color_49A9FF = Color(0xFF49A9FF)
+val Pie_Chart_Color_B8BCC8 = Color(0xFFB8BCC8)
+val Percent_Chart_Color_8E75FF = Color(0xFF8E75FF)
+
+val C_FFFFFF = Color(0xFFFFFFFF)
+val C_F95622 = Color(0xFFF95622)
+val C_FF8F3D = Color(0xFFFF8F3D)
+val C_03A0A2 = Color(0xFF03A0A2)
+val C_43B55C = Color(0xFF43B55C)
+val C_368DED = Color(0xFF368DED)
+val C_CFF5F4 = Color(0xFFCFF5F4)
+val C_FF7878 = Color(0xFFFF7878)
+val C_EAECF0 = Color(0xFFEAECF0)
+val C_F95D5D = Color(0xFFF95D5D)
+val C_FF6F36 = Color(0xFFFF6F36)
